54b12949317e2d8f956c18a1dc7af41e7bd5024a
thomas83470 <estival.t@hotmail.com>
2018-11-04 11:20:37 +0100

    utilisation d'une base de données + DB browser for SQLite, enfin : Utilisation de l'administration de Django

--- dog/truffe/views.py
+++ dog/truffe/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 
-from .mocks import Post
+#from .mocks import Post
+from .models import Post
 
 def index(request):
-	posts = Post.all()
+	posts = Post.objects.all()
 	posts2 = []
 	return render(request, 'truffe/index.html', {'posts' : posts,})
 
 def show(request, id):
-	posts = Post.find(id)
+	posts = Post.object.get(pk=id)
 	return render(request, 'truffe/show.html', {'id': id, 'posts' : posts,})
